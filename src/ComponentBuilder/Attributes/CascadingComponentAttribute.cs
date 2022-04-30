70f8e5ac60c24f33d562f48734f5fe96276f27c8
William <william-zzh@outlook.com>
2022-04-30 18:09:03 +0800

    add comments

--- src/ComponentBuilder/Attributes/CascadingComponentAttribute.cs
+++ src/ComponentBuilder/Attributes/CascadingComponentAttribute.cs
@@ -1,11 +1,24 @@
 ﻿namespace ComponentBuilder.Attributes;
+
+
+/// <summary>
+/// Indicates a cascading component of parameter that has <see cref="CascadingParameterAttribute"/> will be automtically added to the parent component.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class CascadingComponentAttribute : Attribute
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="CascadingComponentAttribute"/> class.
+    /// </summary>
+    /// <param name="componentTypes">The parent component types.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="componentTypes"/> is null.</exception>
     public CascadingComponentAttribute(params Type[] componentTypes)
     {
         ComponentTypes = componentTypes ?? throw new ArgumentNullException(nameof(componentTypes));
     }
 
+    /// <summary>
+    /// Gets the types of parent component.
+    /// </summary>
     public Type[] ComponentTypes { get; }
 }
