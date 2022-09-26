c49988a17d91fcb1a763e3d43ec7520b8d4e45d9
William <william-zzh@outlook.com>
2022-09-26 13:31:28 +0800

    新增方法，优化 CSS 的加载顺序

--- src/ComponentBuilder/Abstrations/Internal/DefaultCssClassBuilder.cs
+++ src/ComponentBuilder/Abstrations/Internal/DefaultCssClassBuilder.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace ComponentBuilder.Abstrations.Internal;
+﻿namespace ComponentBuilder.Abstrations.Internal;
 
 /// <summary>
 /// 默认 <see cref="ICssClassBuilder"/> 的实现。
@@ -21,19 +19,14 @@ public class DefaultCssClassBuilder : ICssClassBuilder
     public IEnumerable<string> CssList => _classes;
 
     /// <inheritdoc/>
-    public ICssClassBuilder Append(string value)
+    public ICssClassBuilder Append(string? value)
     {
-        if (!string.IsNullOrEmpty(value) && !_classes.Contains(value))
+        if (!string.IsNullOrEmpty(value) && !Contains(value))
         {
             _classes.Add(value);
         }
         return this;
     }
-
-    /// <summary>
-    /// 用空格连接 CSS 字符串。
-    /// </summary>
-    public override string ToString() => string.Join(" ", _classes.Distinct());
 
     /// <summary>
     /// 清楚 CSS 列表。
@@ -44,12 +37,47 @@ public class DefaultCssClassBuilder : ICssClassBuilder
     void IDisposable.Dispose() => Clear();
 
     /// <inheritdoc/>
-    public ICssClassBuilder Insert(int index, string value)
+    public ICssClassBuilder Insert(int index, string? value)
     {
         if (!string.IsNullOrEmpty(value) && !_classes.Contains(value))
         {
             _classes.Insert(index, value);
         }
         return this;
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException($"“{nameof(value)}”不能为 null 或空白。", nameof(value));
+        }
+
+        return _classes.Contains(value);
+    }
+
+    /// <inheritdoc/>
+    public bool IsEmpty() => !_classes.Any();
+
+    /// <inheritdoc/>
+    public ICssClassBuilder Remove(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return this;
+        }
+        _classes.Remove(value);
+        return this;
+    }
+
+    /// <summary>
+    /// 用空格连接 CSS 字符串。
+    /// </summary>
+    public override string ToString()
+    {
+        var result = string.Join(" ", _classes.Distinct());
+        //this.Clear();
+        return result;
     }
 }
