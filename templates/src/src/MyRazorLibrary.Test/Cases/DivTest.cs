5b5802687cce8668fd6b41549434c2d88ccb0fdc
William <william-zzh@outlook.com>
2023-02-18 22:37:44 +0800

    update the template

--- templates/src/src/MyRazorLibrary.Test/Cases/DivTest.cs
+++ templates/src/src/MyRazorLibrary.Test/Cases/DivTest.cs
@@ -1,27 +1,29 @@
 namespace MyRazorLibrary.Test.Cases;
 
+//Here are examples to test component
+
 public class DivTest : TestBase<Div>
 {
     [Fact]
     public void Test_Render_Div()
     {
-        GetComponent().Should().HaveTag("div");
+        RenderComponent().Should().HaveTag("div");
     }
 
     [Fact]
     public void Test_Render_With_Content()
     {
-        GetComponent(m => m.AddChildContent("hello")).Should().HaveTag("div").And.HaveChildMarkup("hello");
+        RenderComponent(m => m.AddChildContent("hello")).Should().HaveTag("div").And.HaveChildMarkup("hello");
     }
 
     [Fact]
     public void Test_Has_AdditionalClass()
     {
-        GetComponent(m => m.Add(p => p.AdditionalClass, "test-class")).Should().HaveClass("test-class");
+        RenderComponent(m => m.Add(p => p.AdditionalClass, "test-class")).Should().HaveClass("test-class");
     }
     [Fact]
     public void Test_Has_AdditionalStyle()
     {
-        GetComponent(m => m.Add(p => p.AdditionalStyle, "width:100px;font-size:2rem")).Should().HaveAttribute("style,","width:100px;font-size:2rem");
+        RenderComponent(m => m.Add(p => p.AdditionalStyle, "width:100px;font-size:2rem")).Should().HaveAttribute("style,", "width:100px;font-size:2rem");
     }
 }
--- templates/src/src/MyRazorLibrary.Test/TestBase.cs
+++ templates/src/src/MyRazorLibrary.Test/TestBase.cs
@@ -1,30 +1,62 @@
-﻿using Bunit;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MyRazorLibrary.Test;
-public class TestBase
+/// <summary>
+/// Represents a base class for unit test.
+/// </summary>
+public abstract class TestBase
 {
     private readonly ServiceProvider _provider;
-    private readonly TestContext _testContext;
 
-    public TestBase()
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestBase"/> class.
+    /// </summary>
+    protected TestBase()
     {
-
-        _testContext = new TestContext();
-        _testContext.Services.AddComponentBuilder();
-        _provider = _testContext.Services.BuildServiceProvider();
+        TestContext = new TestContext();
+        ConfigureServices(TestContext.Services);
+        _provider = TestContext.Services.BuildServiceProvider();
     }
 
-    protected TestContext TestContext => _testContext;
+    /// <summary>
+    /// Gets the test context.
+    /// </summary>
+    protected TestContext TestContext { get; private set; }
 
+    /// <summary>
+    /// Gets the service from DI.
+    /// </summary>
+    /// <returns>The service or <c>null</c>.</returns>
     protected TService? GetService<TService>() => _provider.GetService<TService>();
+
+    /// <summary>
+    /// Configure services to DI.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    protected virtual void ConfigureServices(IServiceCollection services)
+    {
+        services.AddComponentBuilder();
+
+        //add your services here
+    }
 }
-
-public class TestBase<TComponent> : TestBase where TComponent : IComponent
+/// <summary>
+/// Represents a base class for unit test of specified component.
+/// </summary>
+/// <typeparam name="TComponent">The type of component.</typeparam>
+public abstract class TestBase<TComponent> : TestBase where TComponent : IComponent
 {
-    protected IRenderedComponent<TComponent> GetComponent(Action<ComponentParameterCollectionBuilder<TComponent>> parameterBuilder) => TestContext.RenderComponent(parameterBuilder);
+    /// <summary>
+    /// Render component with parameters.
+    /// </summary>
+    /// <param name="parameterBuilder">A delegate to provide parameter of component.</param>
+    protected IRenderedComponent<TComponent> RenderComponent(Action<ComponentParameterCollectionBuilder<TComponent>> parameterBuilder) => TestContext.RenderComponent(parameterBuilder);
 
-    protected IRenderedComponent<TComponent> GetComponent(params ComponentParameter[] parameters) => TestContext.RenderComponent<TComponent>(parameters);
+    /// <summary>
+    /// Render component with parameters.
+    /// </summary>
+    /// <param name="parameters">Parameters to provide.</param>
+    protected IRenderedComponent<TComponent> RenderComponent(params ComponentParameter[] parameters) => TestContext.RenderComponent<TComponent>(parameters);
 
 }
