3c76b88fab859e0951db4849b20fa455aa134bb1
William <william-zzh@outlook.com>
2023-02-24 15:51:12 +0800

    feat: ChildContent attribute for fluent tree

--- src/ComponentBuilder/Fluent/IFluentOpenComponentBuilder.cs
+++ src/ComponentBuilder/Fluent/IFluentOpenComponentBuilder.cs
@@ -12,11 +12,4 @@ public interface IFluentOpenComponentBuilder : IDisposable
     /// <returns>The <see cref="IFluentAttributeBuilder"/> instance.</returns>
     IFluentAttributeBuilder Component(Type componentType);
 
-    /// <summary>
-    /// Create an open component of frame with specified component type.
-    /// </summary>
-    /// <typeparam name="TComponent">The type of component.</typeparam>
-    /// <returns>The <see cref="IFluentAttributeBuilder"/> instance.</returns>
-    IFluentAttributeBuilder Component<TComponent>() where TComponent : IComponent
-        => Component(typeof(TComponent));
 }
