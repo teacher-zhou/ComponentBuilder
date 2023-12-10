46a36f88ba13764d5f420daf8cbf1aea10ec9688
William <william-zzh@outlook.com>
2023-12-10 19:40:29 +0800

    slider range

---
+++ src/ComponentBuilder.Enhancements/ParameterException.cs
@@ -0,0 +1,13 @@
+﻿namespace ComponentBuilder;
+
+
+public class ParameterException : Exception
+{
+    public ParameterException():base("The parameter is not valid for component")
+    {
+    }
+
+    public ParameterException(string parameterName):base($"Parameter '{parameterName}' is not valid for component") { }
+
+    public ParameterException(string message,string parameterName) : base($"{message}{Environment.NewLine}Parameter: {parameterName}") { }
+}
